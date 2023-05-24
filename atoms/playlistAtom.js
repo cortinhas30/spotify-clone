dfa54a74522329eb4e76172156c2539add9f79e9
unknown <lucascortinhas.lc@gmail.com>
2023-05-24 19:22:19 -0300

    song component

--- atoms/playlistAtom.js
+++ atoms/playlistAtom.js
@@ -7,5 +7,5 @@ export const playlistState = atom({
 
 export const playlistIdState = atom({
     key:"playlistIdState",
-    default:"4oIs5ekz4GM9mea1UeCMb5"
+    default:"37i9dQZF1EQoowv2cDraCW"
 })
